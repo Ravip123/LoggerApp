23daccb1e73c22736f3c13effb7c3a0e87cf45be
Ravip123 <rpaikine123@gmail.com>
2023-04-08 14:43:57 +0530

    Jenkins

---
+++ src/main/java/com/amz/AmzApplication.java
@@ -0,0 +1,23 @@
+package com.amz;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.*;
+@SpringBootApplication
+public class AmzApplication implements CommandLineRunner {
+public static final Logger Logger= LoggerFactory.getLogger(AmzApplication.class);
+
+public static void main(String[] args) {
+Logger.info("its a CI pipeline job");
+SpringApplication.run(AmzApplication.class, args);
+}
+
+@Override
+public void run(String... args) throws Exception {
+Logger.info("second log");
+
+}
+
+}
---
+++ src/test/java/com/amz/AmzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
